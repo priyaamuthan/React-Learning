041854c0fd6980ad85d705c37bea503f6403a35d
priyaamuthan <162452423+priyaamuthan@users.noreply.github.com>
2025-05-10 23:10:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from './MyComponent'; // Make sure the path is correct
+
+function App() {
+  return (
+    <div>
+      <h2>This is App Component</h2>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Hello.jsx
@@ -0,0 +1,7 @@
+import Hello from ',/components/Hello'
+function Hello(){
+    return (
+        <Hello/>
+    )
+}
+export default Hello;
